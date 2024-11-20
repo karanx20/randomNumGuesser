d36efe0327d11055753c505b76c82b937a264876
Karan Sharma <129947970+karanx20@users.noreply.github.com>
2024-11-20 08:08:41 +0530

    Add files via upload

---
+++ RandomNumGuesser.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+const target = Math.round(Math.random() * 100);
+
+let guesses = 0;
+console.log(target);
+
+while (true) {
+    guesses++;
+
+const guess = Number(prompt("Guess the number (0-100): "));
+if (guess > target) {
+    console.log("Your guess is too high.");
+    continue;
+} else if (guess < target) {
+    console.log("Your guess is too low.");
+    continue;
+} 
+    console.log("You guessed it!");
+    break;
+}
+
+
+console.log("You guessed the number in", guesses, "tries!");
